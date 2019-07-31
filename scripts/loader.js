2fc3c2611c4765efe52debf4594504ece366e6a8
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2019-07-31 09:32:25 +0200

    repaired spinner

---
+++ scripts/loader.js
@@ -0,0 +1,8 @@
+
+window.onload =function() {
+    console.log('loaded');
+    const x = document.getElementById('loader-wrapper');
+    x.remove();
+    
+    };
+    
--- scripts/myscript.js
+++ scripts/myscript.js
@@ -165,11 +165,3 @@ function removeMessage() {
 
 });
 
-
-window.onload =function() {
-  console.log('loaded');
-  const x = document.getElementById('loader-wrapper');
-  x.remove();
-  
-  };
-  

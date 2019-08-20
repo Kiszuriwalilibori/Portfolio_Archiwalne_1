2d98c0f84e5a908b146dc22f72c6d56ef6fef1ec
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2019-08-20 11:35:04 +0200

    added smaller images

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,10 +1,9 @@
 
 window.onload =function() {
-
+    
     if (window.innerWidth < 401) {
         this.console.log(window.innerWidth);
         const project__descriptions = [...document.getElementsByClassName('project__comment')];
-        this.console.log(project__descriptions);
         project__descriptions.forEach(item => item.classList.add('draggable'));
     }
 
